114e81487b8a5800de5fd26cba256e507ce22239
Daniel Delchev <daniel.delchev@yahoo.com>
2024-10-20 01:31:36 +0300

    Setup handlebars

--- NodeJS/movie-magic/README.md
+++ NodeJS/movie-magic/README.md
@@ -1,3 +1,4 @@
 ## Developing steps
 1. Initial Project Architecture
 2. Setup express
+3. Add view engine 
--- NodeJS/movie-magic/src/index.js
+++ NodeJS/movie-magic/src/index.js
@@ -1,10 +1,17 @@
 import express from  'express';
-
+import handlebars from 'express-handlebars';
 const app = express();
 
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+
+}))
+
+app.set('view engine', 'hbs');
+app.set('views', './src/views');
 
 app.get('/', (req,res) => {
-    res.send('works')
+    res.render('index', { layout: false })
 })
 
 app.listen(5000, () => {

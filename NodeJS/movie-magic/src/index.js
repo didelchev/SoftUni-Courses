c5f03c1fca69deb2b901d6c41fa3769be807284d
Daniel Delchev <daniel.delchev@yahoo.com>
2024-10-20 01:07:46 +0300

    Setup express

--- NodeJS/movie-magic/src/index.js
+++ NodeJS/movie-magic/src/index.js
@@ -1,1 +1,13 @@
-console.log("Works");
+import express from  'express';
+
+const app = express();
+
+
+app.get('/', (req,res) => {
+    res.send('works')
+})
+
+app.listen(5000, () => {
+    console.log('Server is listening on http://localhost:5000 ....');
+    
+})

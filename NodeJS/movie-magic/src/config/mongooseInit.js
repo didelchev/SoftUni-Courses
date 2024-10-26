6770b2144d341ce460e7c12433d9f4dfcfd51e3f
Daniel Delchev <daniel.delchev@yahoo.com>
2024-10-26 13:20:32 +0300

    Add DB connection

---
+++ NodeJS/movie-magic/src/config/mongooseInit.js
@@ -0,0 +1,19 @@
+import { connect } from "mongoose";
+
+
+const dbUrl = 'mongodb://localhost:27017/magic-movies';
+
+
+
+export default async function mongooseInit() {
+    
+try {
+    connect(dbUrl);
+    console.log('Successfully connected to DB :) ')
+    
+} catch (error) {
+    console.log('Cannot connect to DB' + error.message);
+    
+}
+    
+}
--- NodeJS/movie-magic/src/index.js
+++ NodeJS/movie-magic/src/index.js
@@ -3,9 +3,10 @@ import express from  'express';
 import routes from './routes.js'
 import handlebarsInit from './config/handlebarsInit.js';
 import exppressInit from './config/expressInit.js';
-
+import mongooseInit from './config/mongooseInit.js';
 const app = express();
 
+mongooseInit();
 handlebarsInit(app);
 exppressInit(app);
 

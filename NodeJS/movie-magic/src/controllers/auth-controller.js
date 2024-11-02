88e6ff50c7deaec570d4b6c0e1a554dcfb52c0d5
Daniel Delchev <daniel.delchev@yahoo.com>
2024-11-02 19:24:07 +0200

    Add hash password

--- NodeJS/movie-magic/src/controllers/auth-controller.js
+++ NodeJS/movie-magic/src/controllers/auth-controller.js
@@ -3,21 +3,25 @@ import authService from "../services/authService.js";
 
 const router = Router();
 
+// GET
 router.get('/register', (req, res ) => {
     res.render('auth/register');
-})
-
-router.post('/register' , async (req,res) => {
-     const { email, password, rePassword} = req.body;
-
-     await authService.register(email, password)
-
-    res.redirect('auth/login');
-
 })
 
 router.get('/login', (req, res ) => {
     res.render('auth/login')
 })
+
+
+// POST
+router.post('/register' , async (req,res) => {
+     const { email, password, rePassword} = req.body;
+
+     await authService.register(email, password)
+
+    res.redirect('/auth/login');
+
+})
+
 
 export default router;
--- NodeJS/movie-magic/src/models/User.js
+++ NodeJS/movie-magic/src/models/User.js
@@ -1,8 +1,23 @@
+import bcrypt from 'bcrypt';
 import { Schema, model } from "mongoose";
+
+const SALT_ROUNDS = 10;
+
 
 const userSchema = new Schema ({
     email: String,
-    password: String,
+    password: {
+        type: String,
+        minLength: [3, "Your password is too short"]
+    }
+})
+
+userSchema.pre('save', async function() {
+    // Hash password before save
+    const hash = await bcrypt.hash(this.password, SALT_ROUNDS);
+
+    this.password = hash;
+
 })
 
 const User = model('User', userSchema);

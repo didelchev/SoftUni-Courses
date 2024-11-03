7a9f91675a3f8f9fbc117b1bb8e0dde8c69ee0a8
Daniel Delchev <daniel.delchev@yahoo.com>
2024-11-03 13:04:13 +0200

    Add logout

--- NodeJS/movie-magic/src/controllers/auth-controller.js
+++ NodeJS/movie-magic/src/controllers/auth-controller.js
@@ -4,33 +4,36 @@ import authService from "../services/authService.js";
 const router = Router();
 
 // GET
-router.get('/register', (req, res ) => {
-    res.render('auth/register');
-})
+router.get("/register", (req, res) => {
+  res.render("auth/register");
+});
 
-router.get('/login', (req, res ) => {
-    res.render('auth/login')
-})
-
+router.get("/login", (req, res) => {
+  res.render("auth/login");
+});
 
 // POST
-router.post('/register' , async (req,res) => {
-     const { email, password, rePassword} = req.body;
+router.post("/register", async (req, res) => {
+  const { email, password, rePassword } = req.body;
 
-     await authService.register(email, password)
+  await authService.register(email, password);
 
-    res.redirect('/auth/login');
-})
+  res.redirect("/auth/login");
+});
 
-router.post('/login', async(req,res) => {
-    const { email, password } = req.body;
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
 
-    const token = await authService.login(email,password);
+  const token = await authService.login(email, password);
 
-    // Add token to cookie
-    res.cookie('auth', token, {httpOnly: true});
-    res.redirect("/");
-})
+  // Add token to cookie
+  res.cookie("auth", token, { httpOnly: true });
+  res.redirect("/");
+});
 
+router.get("/logout", (req, res) => {
+  res.clearCookie("auth");
+  res.redirect("/");
+});
 
 export default router;

21f2939042a2474bc2887302f687bfa193d8feee
Daniel Delchev <daniel.delchev@yahoo.com>
2024-12-01 16:40:08 +0200

    Add login and register page

--- 07. Angular Project Defense/photo-sharing-platform/src/app/app.component.ts
+++ 07. Angular Project Defense/photo-sharing-platform/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './core/footer/footer.component';
 import { HeaderComponent } from './core/header/header.component';
 import { HomeComponent } from './home/home.component';
 import { SharedComponent } from './shared/shared.component';
@@ -7,7 +8,7 @@ import { SharedComponent } from './shared/shared.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HomeComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- 07. Angular Project Defense/photo-sharing-platform/src/app/app.routes.ts
+++ 07. Angular Project Defense/photo-sharing-platform/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  // User routing
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'home', component: HomeComponent },
+];

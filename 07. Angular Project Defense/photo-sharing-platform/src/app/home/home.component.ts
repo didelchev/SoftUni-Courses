10e96b30734f9f917f27580bd00fa1d11bfeb4b1
Daniel Delchev <daniel.delchev@yahoo.com>
2024-12-08 17:31:45 +0200

    Add login & sign up btn in homepage

--- 07. Angular Project Defense/photo-sharing-platform/src/app/home/home.component.ts
+++ 07. Angular Project Defense/photo-sharing-platform/src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../core/footer/footer.component';
 import { HeaderComponent } from '../core/header/header.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
